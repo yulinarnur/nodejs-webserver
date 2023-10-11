71a67e0f3baf51728050e5fbd3e3148b26d82a60
YulinarNR <yulinarnurrahma@gmail.com>
2023-10-11 13:20:26 +0700

    Latihan: Handling Request

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const http = require("http");
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
-
   response.statusCode = 200;
-  response.end("<h1>Halo HTTP Server!</h1>");
+
+  const { method } = request;
+
+  if (method === "GET") {
+    response.end("<h1>Hello!</h1>");
+  }
+
+  if (method === "POST") {
+    response.end("<h1>Hai!</h1>");
+  }
+
+  if (method === "PUT") {
+    response.end("<h1>Bonjour!!</h1>");
+  }
+
+  if (method === "DELETE") {
+    response.end("<h1>Salam!</h1>");
+  }
 };
 
 const server = http.createServer(requestListener);
@@ -15,3 +31,6 @@ const host = "localhost";
 server.listen(port, host, () => {
   console.log(`Server berjalan pada http://${host}:${port}`);
 });
+
+// coba dijalankan menggunakan curl bisa melalui gitbash,seperti:
+// curl -X GET http://localhost:5000 -> <h1>Hello!<h1/>
